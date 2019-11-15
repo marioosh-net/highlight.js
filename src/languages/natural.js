807d5b41454131b67b8c22f5cf2b731e95d41b36
marioosh <marioosh.net@gmail.com>
2019-11-15 11:10:26 +0100

    natural language, more rules

--- src/languages/natural.js
+++ src/languages/natural.js
@@ -8,14 +8,8 @@ Category: natural
 function(hljs) {
   var natural_keywords = 
 `IF
-END-IF
-END-REPEAT
-END-DECIDE
-DEFINE DATA
 GLOBAL USING
 SUBROUTINE
-END-SUBROUTINE
-REDEFINE
 END-DEFINE
 FOR
 END-FOR
@@ -39,17 +33,6 @@ COMPOSE
 COMPRESS
 COMPUTE
 CREATE OBJECT
-DECIDE FOR
-DECIDE ON
-DEFINE CLASS
-DEFINE DATA
-DEFINE FUNCTION
-DEFINE PRINTER
-DEFINE PROTOTYPE
-DEFINE SUBROUTINE
-DEFINE WINDOW
-DEFINE WORK FILE
-DELETE
 DELETE
 DISPLAY
 DIVIDE
@@ -57,18 +40,15 @@ DO
 DOEND
 DOWNLOAD PC FILE
 EJECT
-END
-END TRANSACTION
+^END
 ESCAPE
 EXAMINE
 EXPAND
-FETCH
 FIND
 FOR
 FORMAT
 GET
 GET SAME
-GET TRANSACTION DATA
 HISTOGRAM
 IF
 IF SELECTION
@@ -135,15 +115,49 @@ UPDATE
 UPDATE
 UPDATELOB
 UPLOAD PC FILE
-WRITE TITLE
-WRITE TRAILER
-WRITE WORK FILE
 WRITE
 VIEW
 MASTER-BLOCK
 RETURN
-`
+OR NOT AND
+`;
+
+var multiword_keywords = 
+`VIEW\\s+OF
+END-IF
+END-REPEAT
+END-DECIDE
+MOVE\\s+BY\\s+NAME
+^DEFINE\\s+DATA
+^LOCAL
+^DECIDE\\s+FOR
+DECIDE\\s+ON
+^DEFINE\\s+CLASS
+^DEFINE\\s+DATA
+^DEFINE\\s+FUNCTION
+^DEFINE\\s+PRINTER
+^DEFINE\\s+PROTOTYPE
+^DEFINE\\s+WINDOW
+^DEFINE\\s+WORK FILE
+GET\\s+TRANSACTION\\s+DATA
+END\\s+TRANSACTION
+WRITE\\s+TITLE
+WRITE\\s+TRAILER
+WRITE\\s+WORK\\s+FILE`;
+
+var multiword_meta = 
+`\\s*DEFINE\\s+SUBROUTINE
+\\sFETCH\\s+RETURN
+\\sFETCH
+\\s*END-SUBROUTINE`
+
+var other_re = 
+`\\*PF-KEY`
+
 var natural_keywords_stripped = natural_keywords.replace(/(?:\r\n|\r|\n)/g, ' ');
+var multiword_keywords_re = multiword_keywords.replace(/(?:\r\n|\r|\n)/g, '|');
+var multiword_meta_re = multiword_meta.replace(/(?:\r\n|\r|\n)/g, '|');
+
 var NATURAL_NUMBER_RE = '\\b' +
   '(' +
     '0[bB]([01]+[01_]+[01]+|[01]+)' + // 0b...
@@ -158,32 +172,71 @@ var NATURAL_NUMBER_RE = '\\b' +
     '([eE][-+]?\\d+)?' + // octal, decimal, float
   ')' +
   '[lLfF]?';
-var NATURAL_NUMBER_MODE = {
-  className: 'number',
-  begin: NATURAL_NUMBER_RE,
-  relevance: 0
-};
+  var NATURAL_NUMBER_MODE = {
+    className: 'number',
+    begin: NATURAL_NUMBER_RE,
+    relevance: 0
+  };
+
+  // 1 #ABC (N10),(D),(B4),(P9.2),(N7.2),(P2),(L)
+  var TYPE_DEF = {
+    className: 'type',
+    begin: '\\(([ANPB]\\d+|[NP]\\d+\\.\\d+|D|L)',
+    end: '\\)'
+  };  
+  var TYPE_DEF2 = {
+    begin: '\\(',
+    end: '\\)',
+    contains: [
+      {
+        className: 'type',
+        begin: '([ANPB]\\d+|[NP]\\d+\\.\\d+|D|L)',
+        relevance: 10
+      }
+    ]
+  };
+  var PERFORM_CALLS = {
+    className: 'keyword',
+    begin: 'PERFORM\\s+',
+    end: '',
+    contains: [
+      {
+        className: 'meta',
+        begin: '[A-Z0-9-_]+',
+        relevance: 10
+      }
+    ]
+  };  
+
   return {
-  case_insensitive: true, // language is case-insensitive
-  keywords: natural_keywords_stripped,
-  contains: [
-    {
-      className: 'string',
-      begin: '\'', end: '\''
-    },
-    NATURAL_NUMBER_MODE,
-    {
-      className: 'header',
-      begin: '^\\-{6}\\*', end: '\n'
-    },
-    hljs.COMMENT(
-      '^\\*', // begin
-      '\n' // end
-    ),
-    hljs.COMMENT(
-      '/\\*', // begin
-      '\n' // end
-    )
-  ]
+    case_insensitive: true, // language is case-insensitive
+    keywords: natural_keywords_stripped,
+    contains: [
+      {
+        className: 'string',
+        begin: '\'', end: '\''
+      },
+      PERFORM_CALLS,      
+      {
+        className: 'keyword',
+        begin: multiword_keywords_re, end: ''
+      },      
+      {
+        className: 'meta',
+        begin: multiword_meta_re, end: ''
+      },            
+      {
+        className: 'built_in',
+        begin: other_re, end: ''
+      },
+      NATURAL_NUMBER_MODE,
+      TYPE_DEF,
+      {
+        className: 'header',
+        begin: '^\\-{6}\\*', end: '\n'
+      },
+      hljs.COMMENT('^\\*', '$'),
+      hljs.COMMENT('/\\*', '$')
+    ]
   };
 }
